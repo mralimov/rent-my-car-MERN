e15e2e4810e5e3c20c34bcf75a25154faf3fd289
mralimov <mralimov89@gmail.com>
2021-06-20 23:58:11 -0400

    navbar component style setup

---
+++ rent-my-car/src/app/components/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const NavbarContainer = styled.div`
+min-height: 68px;
+${tw`
+w-full
+max-
+`}
+`;
