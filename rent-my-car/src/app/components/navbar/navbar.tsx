d574a4c59dafa40cd59c6e5c68094a5ea9e32564
mralimov <mralimov89@gmail.com>
2021-06-21 10:39:43 -0400

    NavItems component

--- rent-my-car/src/app/components/navbar/navbar.tsx
+++ rent-my-car/src/app/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Logo } from "../logo/logo";
+import { NavItems } from "./navItems";
 
 const NavbarContainer = styled.div`
 min-height: 68px;
@@ -23,5 +24,6 @@ export function Navbar() {
         <LogoContainer>
             <Logo />
         </LogoContainer>
+        <NavItems />
     </NavbarContainer>
 }
