97f0acb09afc5a7f585e8a20e9623ee84e75142b
mralimov <mralimov89@gmail.com>
2021-06-22 23:07:00 -0400

    update bookcard

--- rent-my-car/src/app/containers/HomePage/homepage.tsx
+++ rent-my-car/src/app/containers/HomePage/homepage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { BookCard } from "../../components/bookCard/bookingCard";
 import { Navbar } from "../../components/navbar/navbar";
 import { TopSection } from "./topsection";
 
@@ -20,5 +21,6 @@ export function HomePage() {
     return  <PageContainer>
                 <Navbar />
                 <TopSection />
+                <BookCard />
             </PageContainer>
 }
